005401ce733464a3b1dbc7a806a653c8eb64a1b2
Alex <sagaidak1337@gmail.com>
2020-05-21 10:07:34 +0300

    Updates

---
+++ precache-manifest.0a745914d51b480496c00f539d41d68f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a9e1cabea19a61bfe4db6720db70c72",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "685485069a9759548744",
+    "url": "/menu/static/css/main.87a5c21b.chunk.css"
+  },
+  {
+    "revision": "cf02692e0b04f7f46077",
+    "url": "/menu/static/js/2.bf9d30d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.bf9d30d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685485069a9759548744",
+    "url": "/menu/static/js/main.bf71616d.chunk.js"
+  },
+  {
+    "revision": "06660c882535303679aa",
+    "url": "/menu/static/js/runtime-main.30d1e7d5.js"
+  },
+  {
+    "revision": "2ed0d4629686162b388193c874c9b9f5",
+    "url": "/menu/static/media/avatar.2ed0d462.svg"
+  },
+  {
+    "revision": "9ebeb518341fe3eaaa27c499f27fde21",
+    "url": "/menu/static/media/img.9ebeb518.svg"
+  },
+  {
+    "revision": "afc9161bc84ee9138024475063fe4d46",
+    "url": "/menu/static/media/logo.afc9161b.svg"
+  }
+]);
